99a8a0a3eebee740d84065661bab36aabd849eed
Hein <heindauven@gmail.com>
2021-12-03 00:13:09 +0100

    Check that image formats are equal.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 mod args;
 use args::Args;
 use image::{ io::Reader, DynamicImage, ImageFormat };
+use std::{ io::BufReader, fs::File };
 
-fn main() {
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
+
+fn main() -> Result<(), ImageDataErrors> {
     let args = Args::new();
 
-    println!("{:?}", args);
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+
+    if image_format_1 != image_format_2 {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+    Ok(())
 }
 
 fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
