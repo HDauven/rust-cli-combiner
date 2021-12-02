2538a4f1212011e19c83a45db8826ce0fdbee1ab
Hein <heindauven@gmail.com>
2021-12-03 00:07:43 +0100

    Add image reading function.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 mod args;
 use args::Args;
+use image::{ io::Reader, DynamicImage, ImageFormat };
 
 fn main() {
     let args = Args::new();
+
     println!("{:?}", args);
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
 }
