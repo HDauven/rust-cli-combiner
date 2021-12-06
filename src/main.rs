ad3f98be6241851c48dde4cda385ab948acb61a4
Hein <heindauven@gmail.com>
2021-12-06 22:29:08 +0100

    Save processed image

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ impl FloatingImage {
             return Err(ImageDataErrors::BufferTooSmall);
         }
         self.data = data;
-        Ok(());
+        Ok(())
     }
 }
 
@@ -48,10 +48,12 @@ fn main() -> Result<(), ImageDataErrors> {
     }
 
     let (image_1, image_2) = standardise_size(image_1, image_2);
-    let output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
+    let mut output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
     
     let combined_data = combine_images(image_1, image_2);
     output.set_data(combined_data)?;
+
+    image::save_buffer_with_format(output.name, &output.data, output.width, output.height, image::ColorType::Rgba8, image_format_1).unwrap();
     Ok(())
 }
 
